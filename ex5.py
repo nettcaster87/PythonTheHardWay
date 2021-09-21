35e1dc9972fdad1c4c59ee3257f55164d10ba257
Khigha87 <nettcaster87@gmail.com>
2021-09-21 23:30:52 +0200

    Add exercises 5 - 8

---
+++ ex5.py
@@ -0,0 +1,41 @@
+Name = 'Khayalethu Ubisi'
+Age = 33
+Height = 1.7 #meters
+Weight = 70 #kilograms
+Eyes = 'Brown'
+Teeth = 'White'
+Hair = 'Black'
+weightInPounds = Weight * 2.54
+heightInInches = Height * 40
+
+print(f"Let's talk about {Name}.")
+print(f"He's {Height} meters tall. Which is {heightInInches} inches.")
+print(f"He's {Weight} kilograms heavy. That is {weightInPounds} pounds.")
+print("Actually that's not too heavy.")
+print(f"He's got {Eyes} eyes and {Hair} hair.")
+print(f"His teeth are usually {Teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = Age + Height + Weight
+print(f"If I add {Age}, {Height} and {Weight} I get {total}")
+
+################################################
+# myName = 'Khayalethu Ubisi'
+# myAge = 33
+# myHeight = 1.7 #meters
+# myWeight = 70 #kilograms
+# myEyes = 'Brown'
+# myTeeth = 'White'
+# myHair = 'Black'
+
+# print(f"Let's talk about {myName}.")
+# print(f"He's {myHeight} meters tall.")
+# print(f"He's {myWeight} kilograms heavy.")
+# print("Actually that's not too heavy.")
+# print(f"He's got {myEyes} eyes and {myHair} hair.")
+# print(f"His teeth are usually {myTeeth} depending on the coffee.")
+
+# #this line is tricky, try to get it exactly right
+# total = myAge + myHeight + myWeight
+# print(f"If I add {myAge}, {myHeight} and {myWeight} I get {total}")
+
